a955f87cdd82f010a0bb7473b3bb4cd4e542736f
백계훈 <gh.back@designgj.com>
2019-12-14 21:21:18 +0900

    code theorem chatlist

--- src/pages/List.vue
+++ src/pages/List.vue
@@ -19,13 +19,6 @@ export default {
     ZHeader,
     Zlist
   },
-  data () {
-    return {
-      returnMessageData: [],
-      messageDataArray: [],
-      userDataArray: []
-    }
-  },
   computed: {
     ...mapGetters(['chatList'])
   },
@@ -34,39 +27,6 @@ export default {
   },
   methods: {
     ...mapActions(['getChatListData'])
-    // async getUsedMessageData () {
-    //   const getMessageData = await require('../api/usedMessageData.json')
-    //   return new Promise((resolve, reject) => {
-    //     if (getMessageData) {
-    //       resolve(getMessageData)
-    //     } else {
-    //       reject(alert('메세지 리스트 데이터가 존재하지 않습니다.'))
-    //     }
-    //   })
-    // },
-    // async getUserInfoData () {
-    //   const getUserData = await require('../api/userInfoData.json')
-    //   return new Promise((resolve, reject) => {
-    //     if (getUserData) {
-    //       resolve(getUserData)
-    //     } else {
-    //       reject(alert('회원 데이터를 가져오지 못했습니다.'))
-    //     }
-    //   })
-    // },
-    // catchUserInfoData () {
-    //   this.getUserInfoData().then((resolveData) => {
-    //     this.userDataArr = resolveData.userInfoData
-    //   })
-    // },
-    // catchUsedMessageData () {
-    //   this.getUsedMessageData().then((resolveData) => {
-    //     this.messageDataArr = resolveData.usedMessageData
-    //   })
-    // },
-    // mergeChatListData () {
-    //   console.log(this.messageDataArr)
-    // }
   }
 }
 </script>
